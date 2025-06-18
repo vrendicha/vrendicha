840454759aa4da58f3b4972580bcf12c805a070a
Vrendicha <vrendichaarieshadhiegustaf@gmail.com>
2025-06-18 12:07:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 
