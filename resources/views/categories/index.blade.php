fd7abb56d30ea23aef661fe7813f3fa6a3f7808b
Vrendicha <vrendichaarieshadhiegustaf@gmail.com>
2025-06-18 09:43:00 +0700

    Add files via upload

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,12 @@
+{{-- resources/views/categories/index.blade.php --}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Daftar Kategori') }}
+        </h2>
+    </x-slot>
+
+    <div class="p-6">
+        <p>Ini adalah halaman daftar kategori.</p>
+    </div>
+</x-app-layout>
