f6c344867ff66be279ff9fa3e2d1c3c6229e955f
Vrendicha <vrendichaarieshadhiegustaf@gmail.com>
2025-07-02 08:44:03 +0700

    Delete home.blade.php

--- home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Selamat datang di E-Commerce kami!</h1>
-    <p>Temukan produk terbaik kami!</p>
-    <a href="{{ route('products.index') }}">Lihat Produk</a>
-@endsection
